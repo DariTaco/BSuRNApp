a4b15159c9a0f6a3f09941ff8336e73b571468e9
dariakern <CuLa.LoU@hotmail.de>
2019-11-02 15:11:22 +0200

    tabbed page

--- WertheApp/RN/DijkstraSettings.cs
+++ WertheApp/RN/DijkstraSettings.cs
@@ -1,30 +1,63 @@
 ﻿using System;
 using System.Diagnostics; //Debug.WriteLine("");
 using Xamarin.Forms;
+using SkiaSharp.Views.Forms;
 namespace WertheApp.RN
 {
     public class DijkstraSettings: TabbedPage
     {
+        //VARIABLES
+        private DijkstraSettingsDraw skiaview0, skiaview1, skiaview2, skiaview3;
+        private SKCanvasView canvas0, canvas1, canvas2, canvas3;
+
+        //CONSTRUCTOR
         public DijkstraSettings()
         {
-            //Child Pages
-            var child1 = new NavigationPage(new DijkstraSettingsChild1("Network 1"));
-            var child2 = new NavigationPage(new DijkstraSettingsChild1("Network 2"));
-            var child3 = new NavigationPage(new DijkstraSettingsChild1("Network 3"));
-            var child4 = new NavigationPage(new DijkstraSettingsChild1("Network 4"));
+            Title = "Dijkstra";
 
-            //Titles for Tabs
-            child1.Title = "child1";
-            child2.Title = "child2";
-            child3.Title = "child3";
-            child4.Title = "child4";
+            CreateContent();
             //navigationPage.IconImageSource = "schedule.png";
+        }
 
-            //Add Tabs with Child Pages
+        //METHODS
+        /**********************************************************************
+        *********************************************************************/
+        void CreateContent()
+        {
+            //get canvas to draw on
+            skiaview0 = new DijkstraSettingsDraw();
+            canvas0 = skiaview0.ReturnCanvas();
+            skiaview1 = new DijkstraSettingsDraw();
+            canvas1 = skiaview1.ReturnCanvas();
+            skiaview2 = new DijkstraSettingsDraw();
+            canvas2 = skiaview2.ReturnCanvas();
+            skiaview3 = new DijkstraSettingsDraw();
+            canvas3 = skiaview3.ReturnCanvas();
+
+            //Child Pages (Tabs)
+            var child0 = new ContentPage();
+            var child1 = new ContentPage();
+            var child2 = new ContentPage();
+            var child3 = new ContentPage();
+
+            //assign canvas to Tab
+            child0.Content = canvas0;
+            child1.Content = canvas1;
+            child2.Content = canvas2;
+            child3.Content = canvas3;
+
+            //Tab titles
+            child0.Title = "network 1";
+            child1.Title = "network 2";
+            child2.Title = "network 3";
+            child3.Title = "network 4";
+
+            //Add Tabs to TabbedPage
+            Children.Add(child0);
             Children.Add(child1);
             Children.Add(child2);
             Children.Add(child3);
-            Children.Add(child4);
+
         }
     }
 }
---
+++ WertheApp/RN/DijkstraSettingsDraw.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Xamarin.Forms;
+using SkiaSharp.Views.Forms;
+using SkiaSharp;
+using System.Diagnostics; //Debug.WriteLine("");
+using System.Collections.Generic;
+
+namespace WertheApp.RN
+{
+    public class DijkstraSettingsDraw
+    {
+        //VARIABLES
+        private SKCanvasView skiaview;
+        private static float xe, ye;
+
+        private static SKPaint sk_blackText, sk_test;
+        private static float textSize, strokeWidth;
+
+        //CONSTRUCTOR
+        public DijkstraSettingsDraw()
+        {
+            // crate the canvas
+            this.skiaview = new SKCanvasView();
+            this.skiaview.PaintSurface += PaintSurface;
+
+            textSize = 5;
+            strokeWidth = 0.2f;
+        }
+
+        //METHODS
+        //METHODS
+        /**********************************************************************
+        *********************************************************************/
+        // do the drawing
+        void PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            //canvas object
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            //Important! Otherwise the drawing will look messed up in iOS
+            if (canvas != null){ canvas.Clear();}
+
+            //calculate some stuff and make the paint
+            CalculateNeededNumbers(canvas);
+            MakeSKPaint(); //depends on xe and ye and therfore has to be called after they were initialized
+
+            /*********************HERE GOES THE DRAWING************************/
+            SKRect sk_rBackground = new SKRect(00 * xe, 0 * ye, 100 * xe, 100 * ye); //left , top, right, bottom
+            canvas.DrawRect(sk_rBackground, sk_test); //left, top, right, bottom, color
+
+            //execute all drawing actions
+            canvas.Flush();
+        }
+
+
+        /**********************************************************************
+        *********************************************************************/
+        static void CalculateNeededNumbers(SKCanvas canvas)
+        {
+            /*important: the coordinate system starts in the upper left corner*/
+            float lborder = canvas.LocalClipBounds.Left;
+            float tborder = canvas.LocalClipBounds.Top;
+            float rborder = canvas.LocalClipBounds.Right;
+            float bborder = canvas.LocalClipBounds.Bottom;
+
+            xe = rborder / 100; //using the variable surfacewidth instead would mess everything up
+            ye = bborder / 100;
+        }
+
+        /**********************************************************************
+        *********************************************************************/
+        public SKCanvasView ReturnCanvas()
+        {
+            return this.skiaview;
+        }
+
+        /**********************************************************************
+        *********************************************************************/
+        //redraws the canvas
+        public void Paint()
+        {
+            // update the canvas when the data changes
+            this.skiaview.InvalidateSurface();
+
+        }
+
+        /**********************************************************************
+        *********************************************************************/
+        static private void MakeSKPaint()
+        {
+            //For Text
+            sk_blackText = new SKPaint
+            {
+                Color = SKColors.Black,
+                TextSize = ye * textSize / 1.8f,
+                IsAntialias = true,
+                IsStroke = false, //TODO: somehow since the newest update this doesnt work anymore for ios
+                TextAlign = SKTextAlign.Center,
+                IsVerticalText = false
+            };
+
+            sk_test = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                StrokeWidth = 5,
+                IsAntialias = true,
+                Color = new SKColor(67, 110, 238).WithAlpha(30)
+            };
+        }
+
+    }
+}
