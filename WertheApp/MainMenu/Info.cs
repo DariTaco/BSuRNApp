5cf5257c613d563505a9bdff11b07d773eb6eb92
dariakern <CuLa.LoU@hotmail.de>
2020-11-18 18:19:36 +0200

    #

--- WertheApp/MainMenu/Info.cs
+++ WertheApp/MainMenu/Info.cs
@@ -1,8 +1,23 @@
 ﻿using System;
 using Xamarin.Essentials;
-
-
 using Xamarin.Forms;
+
+
+
+/*
+ In der Historie Datumswerte wie folgt angeben:
+
+dd-mmm-yyyy, also heute: 15-Nov-2020,
+
+d.h. Tageswerte zweistellig, Monatsangaben wie folgt:
+Jan, Feb, Mar, Apr, Jun, Jul, Aug, Sep, Oct, Nov, Dec
+Jahreszahlen vierstellig.
+
+Ganz unten ein Datum mit Versionierung eingeführt angeben.
+Hinter jeden Eintrag daneben auch die Versionsnummer angeben.
+ 
+ 
+ */
 
 namespace WertheApp
 {
@@ -22,14 +37,16 @@ namespace WertheApp
 
             var l_changes = new Label { Text = "changes: ", FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) };
             stackL.Children.Add(l_changes);
-            var l_08112020 = new Label { Text = "11/08/2020: dark mode", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) };
+            var l_08112020 = new Label { Text = "08-Nov-2020: dark mode", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) };
             stackL.Children.Add(l_08112020);
-            var l_03112020 = new Label { Text = "11/03/2020: bug fixes", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) };
+            var l_03112020 = new Label { Text = "03-Nov-2020: bug fixes", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) };
             stackL.Children.Add(l_03112020);
-            var l_25102020 = new Label { Text = "10/25/2020: restart buttons added", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) };
+            var l_25102020 = new Label { Text = "25-Oct-2020: restart buttons added", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) };
             stackL.Children.Add(l_25102020);
-            var l_25102020b = new Label { Text = "10/25/2020: Deadlock (new)", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) };
+            var l_25102020b = new Label { Text = "25-Oct-2020: Deadlock (new)", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) };
             stackL.Children.Add(l_25102020b);
+            var l_24102020b = new Label { Text = "24-Oct-2020: versioning introduced", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) };
+            stackL.Children.Add(l_24102020b);
             VersionTracking.Track();
         }
     }
